faa60b49ad15b97b47a9443030533c0511c90643
YOA\yoaas <syogaadi75@gmail.com>
2024-07-23 23:09:53 +0700

    first commit

--- api/index.php
+++ api/index.php
@@ -1,7 +1,4 @@
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$app->run();
+// Forward Vercel requests to normal index.php
+require __DIR__ . '/../public/index.php';
